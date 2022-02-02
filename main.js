d69fc51d2e6dbec1d7e95a9f15af6dc04af0f651
Filip Jerga <jerga99@gmail.com>
2022-02-02 11:59:24 +0000

    let

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 
 
 // special keywords, const -> variable where you can store values
+//                   let -> variable
 // -------- START OF PROGRAM --------
 const sum = 7 + 7; // +, -, *, / -> number
-const floaty = 7.3 + 8.9; // 16.2 -> number
+let floaty = 7.3 + 8.9; // 16.2 -> number
 const message = "Hello World, today is a beutiful day"; // -> string
-const welcomeText = "Hello" + " " + "Students";
+let welcomeText = "Hello" + " " + "Students";
+let pi = 3.14;
 const truthValue = true; // boolean
 const falseValue = false; //boolean
 
+
+floaty = 10000;
+welcomeText = "Hello" + " " + "Filip";
+
+// pi = pi + 10;
+pi += 10;
 
 console.log(sum);
 console.log(floaty);
@@ -16,4 +24,5 @@ console.log(message);
 console.log(welcomeText);
 console.log(truthValue);
 console.log(falseValue);
+console.log(pi);
 // -------- END OF PROGRAM --------
