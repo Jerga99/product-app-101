b31bf1633f64a1cbd097f259d6b7d6eab0b21273
Filip Jerga <jerga99@gmail.com>
2022-02-02 11:48:55 +0000

    Primitive Types part 2

--- main.js
+++ main.js
@@ -2,11 +2,18 @@
 
 // special keywords, const -> variable where you can store values
 // -------- START OF PROGRAM --------
-const sum = 7 + 7; // 14
-const floaty = 7.3 + 8.9; // 16.2
-const message = "Hello World, today is a beutiful day";
+const sum = 7 + 7; // +, -, *, / -> number
+const floaty = 7.3 + 8.9; // 16.2 -> number
+const message = "Hello World, today is a beutiful day"; // -> string
+const welcomeText = "Hello" + " " + "Students";
+const truthValue = true; // boolean
+const falseValue = false; //boolean
+
 
 console.log(sum);
 console.log(floaty);
 console.log(message);
+console.log(welcomeText);
+console.log(truthValue);
+console.log(falseValue);
 // -------- END OF PROGRAM --------
