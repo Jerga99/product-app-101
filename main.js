22c8d0b1457ba194595ebccfa5d3daa496dc7264
Filip Jerga <jerga99@gmail.com>
2022-02-02 11:37:39 +0000

    Primite Types part 1

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
 
+// special keywords, const -> variable where you can store values
+// -------- START OF PROGRAM --------
+const sum = 7 + 7; // 14
+const floaty = 7.3 + 8.9; // 16.2
+const message = "Hello World, today is a beutiful day";
 
-alert("Hello World");
+console.log(sum);
+console.log(floaty);
+console.log(message);
+// -------- END OF PROGRAM --------
