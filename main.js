60d4cedb568d15e29bfbbef44b2db110b73d3be0
Filip Jerga <jerga99@gmail.com>
2022-02-02 12:13:51 +0000

    functions

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
 
 
-// special keywords, const -> variable where you can store values
-//                   let -> variable
-// -------- START OF PROGRAM --------
-const sum = 7 + 7; // +, -, *, / -> number
-let floaty = 7.3 + 8.9; // 16.2 -> number
-const message = "Hello World, today is a beutiful day"; // -> string
-let welcomeText = "Hello" + " " + "Students";
-let pi = 3.14;
-const truthValue = true; // boolean
-const falseValue = false; //boolean
+
+// function declaration
+function computeRectangleArea() {
+  // 2 variable a and b
+  // compute area of rectangle
+  // console.log the results
+
+  const a = 10;
+  const b = 5;
+  const area = a * b;
+  console.log(area);
+}
 
 
-floaty = 10000;
-welcomeText = "Hello" + " " + "Filip";
-
-// pi = pi + 10;
-pi += 10;
-
-console.log(sum);
-console.log(floaty);
-console.log(message);
-console.log(welcomeText);
-console.log(truthValue);
-console.log(falseValue);
-console.log(pi);
-// -------- END OF PROGRAM --------
+// function execution
+computeRectangleArea();
+computeRectangleArea();
+computeRectangleArea();
+computeRectangleArea();
+computeRectangleArea();
+computeRectangleArea();
