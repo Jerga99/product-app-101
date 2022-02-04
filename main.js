e95db7e1225fa265556cda80ed8e1ff4c4e8e944
Filip Jerga <jerga99@gmail.com>
2022-02-04 10:02:19 +0000

    Function to create product

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 
-// it' a structure that can hold multiple values grouped together
-// key - value pairs
-debugger
-const product1 = {
-  name: "Laptop",
-  type: "eletronics",
-  price: 200,
-  currency: "usd",
-  image: "some_url_of_image"
-}
 
-const product2 = {
-  name: "Super shoes",
-  type: "clothing",
-  price: 50,
-  currency: "usd",
-  image: "some_url_of_image"
-}
 
 function displayProductInfo(product) {
   console.log("Name: ", product.name);
@@ -26,6 +9,19 @@ function displayProductInfo(product) {
   console.log("Image: " + product.image);
   console.log("------------------------")
 }
+
+function createProduct(name, type, price, currency, image) {
+  return {
+    name, // name: name
+    type,
+    price,
+    currency,
+    image
+  }
+}
+
+const product1 = createProduct("Laptop", "eletronics", 500, "usd", "some_url_of_image");
+const product2 = createProduct("Super shoes", "clothing", 50, "usd", "some_url_of_image");
 
 displayProductInfo(product1);
 displayProductInfo(product2);
