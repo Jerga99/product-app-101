d6f2a4d92c9e79a08cdbc43a199f10bc25996f38
Filip Jerga <jerga99@gmail.com>
2022-02-04 10:13:00 +0000

    Default parameters

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ function displayProductInfo(product) {
   console.log("------------------------")
 }
 
-function createProduct(name, type, price, currency, image) {
+function createProduct(
+  name = "Default Name",
+  type,
+  price,
+  currency = "usd",
+  image = "unsplash_url"
+) {
   return {
     name, // name: name
     type,
@@ -22,6 +28,13 @@ function createProduct(name, type, price, currency, image) {
 
 const product1 = createProduct("Laptop", "eletronics", 500, "usd", "some_url_of_image");
 const product2 = createProduct("Super shoes", "clothing", 50, "usd", "some_url_of_image");
+const product3 = createProduct();
+const product4 = createProduct("Fancy Hat", "clothing", 100);
+const product5 = createProduct("Skateboard");
+
 
 displayProductInfo(product1);
 displayProductInfo(product2);
+displayProductInfo(product3);
+displayProductInfo(product4);
+displayProductInfo(product5);
