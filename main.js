6672fcc4f788e8a4a93072f30c4d6170ece47d43
Filip Jerga <jerga99@gmail.com>
2022-02-05 20:39:02 +0000

    Logical operators

--- main.js
+++ main.js
@@ -40,54 +40,20 @@ const mixedList = [1, "Lion", 40, true, 3.14, function(){}, {name: "Filip"}];
 
 const products = [product1, product2, product3, product4, product5];
 
-const index = 4;
-
-const conditional1 = 100 > 150;
-const conditional2 = 3 < 10;
-const conditional3 = 3 === 2;
-
-const conditional4 = 3 == "3";
-const conditional5 = 11 >= 11;
-const conditional6 = 15 <= 16;
-const conditional7 = product1.price > product3.price;
-const conditional8 = products.length < 10;
-// tricky one: you can call "length" on string
-const conditional9 = "Hello There".length < 8;
-const conditional10 = products[1].name.length > 10;
-// tricky one, you can think of true as 1 (something is turned on)
-// false as 0 (something is turned off)
-const conditional11 = true > false;
 
 
+// if ((product1.price > product2.price) && (product3.price > product4.price)) {
+//   console.log("yey");
+// } else {
+//   console.log("no");
+// }
 
-if (100 > 150) {
-  console.log("150 is larger than 100");
-}
-
-if (conditional3) {
-  console.log("3 === 2");
+if (
+  (numberList[0] > numberList[1]) ||
+  (numberList[numberList.length - 1] > numberList[numberList.length - 2])
+) {
+  console.log("yey");
 } else {
-  console.log("3 is not equal to 2");
+  console.log("non");
 }
 
-const justSomeVariable = false;
-
-if (product1.price < product2.price) {
-  console.log("product1 price is > than product2 price")
-} else if (product2.price > product3.price) {
-  console.log("product2 price is > than product3 price")
-} else if (justSomeVariable) {
-  console.log("just some variable");
-} else {
-  console.log("Nothing true so we are in else!");
-}
-
-if (false) {
-  console.log("");
-} else if (false) {
-  console.log("");
-} else if (false) {
-  console.log("")
-} else {
-  console.log("");
-}
