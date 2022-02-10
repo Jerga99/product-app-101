caf069f3361a85f06db002ec32f4029e31bf0905
Filip Jerga <jerga99@gmail.com>
2022-02-10 14:13:19 +0000

    Class

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 
 
 
+class Product {
+  constructor(
+    name = "Default Name",
+    type,
+    price,
+    currency = "usd",
+    image = "unsplash_url"
+  ) {
+    this.name = name;
+    this.type = type;
+    this.price = price;
+    this.currency = currency;
+    this.image = image;
+  }
 
-function Product(
-  name = "Default Name",
-  type,
-  price,
-  currency = "usd",
-  image = "unsplash_url"
-) {
-
-  this.name = name;
-  this.type = type;
-  this.price = price;
-  this.currency = currency;
-  this.image = image;
-  this.displayInfo = function() {
+  displayInfo() {
     console.log("Name: " + this.name);
     console.log("Type: " + this.type);
     console.log("Price: " + this.price);
@@ -24,35 +25,20 @@ function Product(
   }
 }
 
-Product.prototype.displayInfo2 = function() {
-  console.log(this.name + " " + this.type);
-}
-
-
 const product1 = new Product("Laptop", "eletronics", 500, "usd", "some_url_of_image");
 const product2 = new Product("Super shoes", "clothing", 50, "usd", "some_url_of_image");
 const product3 = new Product();
 const product4 = new Product("Fancy Hat", "clothing", 100);
 const product5 = new Product("Skateboard");
 
+
 const products = [product1, product2, product3, product4, product5];
 
-products[0].name = "Phone";
-
-const product100 = product1;
-
-product100.image = "No Image";
-
-console.log(products[0].image);
-console.log(product1.image);
-console.log(product100.image);
-
-// for (let i = 0; i < products.length; i++) {
-//   const product = products[i];
-//   product.displayInfo();
-//   product.displayInfo2();
-//   console.log("--------------------");
-// }
+for (let i = 0; i < products.length; i++) {
+  const product = products[i];
+  product.displayInfo();
+  console.log("--------------------");
+}
 
 
 
