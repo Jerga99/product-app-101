08debde1d80525d5bb2d3c03904ffc63f8111d70
Filip Jerga <jerga99@gmail.com>
2022-02-11 11:35:52 +0000

    Assignment 4

---
+++ assignments/assignment4.js
@@ -0,0 +1,20 @@
+alert("Assignment4")
+
+// 1. create a "Person" class that will create a "person" instance
+//    that will have "name", "age", "nationality"
+//    and a function that will display all user informations
+
+// 2. create at least 4 "person" objects with ages, 18, 23, 40, 45
+
+// 3. create an array, and add all persons you have created before
+//    in that list
+
+// 4. create a for loop and iterate through all persons
+
+// 5. display person informations(name, age, nationality) in for loop
+//    for all persons older than 25 years
+//    - if a person name is shorter than 6 characters display console.log
+//      "<person name> is not valid!"
+
+// CODE UNDER THIS LINE
+// --------------------
