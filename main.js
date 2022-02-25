05567cb9691b349ebf0ab1ce5ebf5170e948876c
Filip Jerga <jerga99@gmail.com>
2022-02-25 08:40:16 +0000

    Use ForEach

--- main.js
+++ main.js
@@ -16,63 +16,23 @@ function runApp() {
 
   const createProductElement = () => {
     const productElement = document.createElement("div");
-
-    const productField1 = document.createElement("div");
-    const productField2 = document.createElement("div");
-    const productField3 = document.createElement("div");
-    const productField4 = document.createElement("div");
-    const productField5 = document.createElement("div");
-
-    const productLabel1 = document.createElement("span");
-    const productLabel2 = document.createElement("span");
-    const productLabel3 = document.createElement("span");
-    const productLabel4 = document.createElement("span");
-    const productLabel5 = document.createElement("span");
-
     productElement.classList.add("product");
 
-    productField1.classList.add("product-field");
-    productField2.classList.add("product-field");
-    productField3.classList.add("product-field");
-    productField4.classList.add("product-field");
-    productField5.classList.add("product-field");
+    const productLabels = ["Name", "Type", "Price", "Currency", "Image"];
+    productLabels.forEach(label => {
+      const productField = document.createElement("div");
+      const productLabel = document.createElement("span");
 
-    productLabel1.classList.add("product-label");
-    productLabel2.classList.add("product-label");
-    productLabel3.classList.add("product-label");
-    productLabel4.classList.add("product-label");
-    productLabel5.classList.add("product-label");
+      productField.classList.add("product-field");
+      productLabel.classList.add("product-label");
 
-    productLabel1.innerHTML = "Name:";
-    productLabel2.innerHTML = "Type:";
-    productLabel3.innerHTML = "Price:";
-    productLabel4.innerHTML = "Currency:";
-    productLabel5.innerHTML = "Image:";
+      productLabel.innerHTML = `${label}:`;
+      const text = document.createTextNode("In Progress");
 
-    const text1 = document.createTextNode("Laptop");
-    const text2 = document.createTextNode("Electronics");
-    const text3 = document.createTextNode("500");
-    const text4 = document.createTextNode("usd");
-    const text5 = document.createTextNode("some url of image");
-
-    productField1.appendChild(productLabel1);
-    productField1.appendChild(text1);
-
-    productField2.appendChild(productLabel2);
-    productField2.appendChild(text2);
-
-    productField3.appendChild(productLabel3);
-    productField3.appendChild(text3);
-    productField4.appendChild(productLabel4);
-    productField4.appendChild(text4);
-    productField5.appendChild(productLabel5);
-    productField5.appendChild(text5);
-
-    productElement.appendChild(productField1);
-    productElement.appendChild(productField2);
-    productElement.appendChild(productField3);
-    productElement.appendChild(productField4);
-    productElement.appendChild(productField5);
+      productField.appendChild(productLabel);
+      productField.appendChild(text);
+      productElement.appendChild(productField);
+    })
 
     return productElement;
   }
