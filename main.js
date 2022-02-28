e6d99d37bdf2148a93213047813ae693e2835dc9
Filip Jerga <jerga99@gmail.com>
2022-02-28 09:36:30 +0000

    Modularity

--- main.js
+++ main.js
@@ -40,31 +40,6 @@ function runApp() {
 
   const products = [product1, product2, product3, product4];
 
-  const _createElement = (elementType, classList) => {
-    const element = document.createElement(elementType);
-    element.classList.add(classList);
-    return element;
-  }
-
-  const createProductElement = (product) => {
-    const productElement = _createElement("div", "product");
-
-    const productLabels = ["Name", "Type", "Price", "Currency", "Image"];
-    productLabels.forEach(label => {
-      const productField = _createElement("div", "product-field");
-      const productLabel = _createElement("span", "product-label");
-
-      productLabel.innerHTML = `${label}:`;
-      const text = document.createTextNode(product[label.toLowerCase()]);
-
-      productField.appendChild(productLabel);
-      productField.appendChild(text);
-      productElement.appendChild(productField);
-    })
-
-    return productElement;
-  }
-
   const container = document.getElementById("productContainer");
 
   products.forEach(product => {
