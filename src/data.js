64e68cdc6a12757e172815e7abccbd4df9332304
Filip Jerga <jerga99@gmail.com>
2022-03-02 09:24:29 +0000

    Move Data

---
+++ src/data.js
@@ -0,0 +1,27 @@
+
+
+const PRODUCT_DATA = [{
+  name: "Laptop",
+  type: "Electronics",
+  price: 500,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Super shoes",
+  type: "Clothing",
+  price: 50,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Smartphone",
+  type: "Phones",
+  price: 300,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Fancy Hat",
+  type: "Clothing",
+  price: 100,
+  currency: "usd",
+  image: "some_url_of_image"
+}]
--- src/main.js
+++ src/main.js
@@ -1,37 +1,10 @@
 
 
-const data = [{
-  name: "Laptop",
-  type: "Electronics",
-  price: 500,
-  currency: "usd",
-  image: "some_url_of_image"
-}, {
-  name: "Super shoes",
-  type: "Clothing",
-  price: 50,
-  currency: "usd",
-  image: "some_url_of_image"
-}, {
-  name: "Smartphone",
-  type: "Phones",
-  price: 300,
-  currency: "usd",
-  image: "some_url_of_image"
-}, {
-  name: "Fancy Hat",
-  type: "Clothing",
-  price: 100,
-  currency: "usd",
-  image: "some_url_of_image"
-}]
-
-
 runApp();
 
 function runApp() {
 
-  const products = data.map(item =>
+  const products = PRODUCT_DATA.map(item =>
     new Product(item.name, item.type, item.price, item.currency, item.image)
   );
 
