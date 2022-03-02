ccf9eba8a337527f6e45f8f55efa07374af96e06
Filip Jerga <jerga99@gmail.com>
2022-03-02 09:21:25 +0000

    Procide raw data

--- src/main.js
+++ src/main.js
@@ -1,16 +1,39 @@
 
 
+const data = [{
+  name: "Laptop",
+  type: "Electronics",
+  price: 500,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Super shoes",
+  type: "Clothing",
+  price: 50,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Smartphone",
+  type: "Phones",
+  price: 300,
+  currency: "usd",
+  image: "some_url_of_image"
+}, {
+  name: "Fancy Hat",
+  type: "Clothing",
+  price: 100,
+  currency: "usd",
+  image: "some_url_of_image"
+}]
+
 
 runApp();
 
 function runApp() {
 
-  const product1 = new Product("Laptop", "eletronics", 500, "usd", "some_url_of_image");
-  const product2 = new Product("Super shoes", "clothing", 50, "usd", "some_url_of_image");
-  const product3 = new Product("Smartphone", "phones", 300, "usd", "some_url_of_image");
-  const product4 = new Product("Fancy Hat", "clothing", 100, "usd", "some_url_of_image");
-
-  const products = [product1, product2, product3, product4];
+  const products = data.map(item =>
+    new Product(item.name, item.type, item.price, item.currency, item.image)
+  );
 
   const container = document.getElementById("productContainer");
 
