72bea20a2c375d3f677a90b83f02dcccc320c73c
Filip Jerga <jerga99@gmail.com>
2022-05-02 09:12:27 +0100

    Framework + Modularity

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -5,6 +5,7 @@ function ProductPage({children}) {
   return `
     <div class="page">
       <div class="container">
+        <h1>My Items</h1>
         <div class="product-grid">
           ${children.join(" ")}
         </div>
--- src/reactivity.js
+++ src/reactivity.js
@@ -1,13 +1,30 @@
 
 
 
-const ReactivityDOM = {
-  render(child) {  
+const ReactivityDOM = (function() {
+  function render(child) {  
     const app = document.getElementById("app");
+    veryUnsecureFunction();
 
     app.insertAdjacentHTML(
       "beforeend", 
       child
     ); 
   }
-}
+
+  function veryUnsecureFunction() {
+    console.log("I am doing something important");
+  }
+
+  // API
+  return {
+    render
+  }
+})()
+
+
+// This is our tiny example framework, we can call ReactivityDOM
+// Framework decides the rules how to work with framework, 
+// what functions are accesible (API)
+// Benefits: easier to work, to collaborate, faster to develop,
+// you can focus on bussines logic of your app instead of handling DOM or html elements
