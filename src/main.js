83029ebb5558f4ff7f1db72a69a64cc5f1ac982c
Filip Jerga <jerga99@gmail.com>
2022-05-02 08:50:16 +0100

    Hide functions

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 
 
-runApp();
 
-async function runApp() {
-  try {
-    const products = await fetchProducts();
-    const children = products.map(product => ProductItem({product}))
-    ReactivityDOM.render(ProductPage({children}));
-  } catch(error) {
-    console.error(error.message);
+(function() {
+  runApp();
+
+  async function runApp() {
+    try {
+      const products = await fetchProducts();
+      const children = products.map(product => ProductItem({product}))
+      ReactivityDOM.render(ProductPage({children}));
+    } catch(error) {
+      console.error(error.message);
+    }
   }
-}
-
+})()
 
