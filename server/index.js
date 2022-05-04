0fb1a2170d19aeecd193e496e0188cac83b3e786
Filip Jerga <jerga99@gmail.com>
2022-05-04 15:29:07 +0100

    Create simple server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 
-const { uniqueNamesGenerator, animals } = require('unique-names-generator');
 
-const randomName = uniqueNamesGenerator({ 
-  dictionaries: [animals] 
+const http = require("http");
+
+const server = http.createServer(function(req, res) {
+  console.log("Wooohoooo!!!");
 });
 
-console.log(randomName);
+server.listen(8080);
+console.log("Server is running on port 8080");
+
